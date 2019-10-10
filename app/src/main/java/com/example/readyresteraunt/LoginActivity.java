b983100a5b695eeb183cd60a876d3fe131cc5c5b
Francisco Chiocconi <franciscochiocconi@gmail.com>
2019-10-09 22:58:27 -0700

    orphan branch

---
+++ app/src/main/java/com/example/readyresteraunt/LoginActivity.java
@@ -0,0 +1,47 @@
+package com.example.readyresteraunt;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+    private Button button;
+    private Button button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        button = findViewById(R.id.loginbutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLogin();
+            }
+        });
+
+        button2 = findViewById(R.id.loginbutton);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRestaraunts();
+            }
+        });
+
+
+    }
+    public void openRestaraunts(){
+        Intent intent = new Intent(getBaseContext(), Restaraunts.class);
+        startActivity(intent);
+    }
+
+    public void openLogin(){
+        Intent intent = new Intent(getBaseContext(), MainActivity.class);
+        startActivity(intent);
+    }
+
+
+}
