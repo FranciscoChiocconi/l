a8d0acd3f9e6ea2b4eec911d46e260bf5c3dac4f
Francisco Chiocconi <franciscochiocconi@gmail.com>
2019-10-10 22:07:32 -0700

    almost final version

--- app/src/main/java/com/example/readyresteraunt/SettingsActivity.java
+++ app/src/main/java/com/example/readyresteraunt/SettingsActivity.java
@@ -1,5 +1,6 @@
 package com.example.readyresteraunt;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,12 +10,36 @@ import androidx.appcompat.app.AppCompatActivity;
 public class SettingsActivity extends AppCompatActivity {
 
     private Button button;
+    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-
+        button = findViewById(R.id.signout);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                opentoLogin();
+            }
+        });
+        button2 = findViewById(R.id.button18);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                save();
+            }
+        });
     }
+    public void opentoLogin(){
+        Intent intent = new Intent(getBaseContext(), MainActivity.class);
+        startActivity(intent);
+    }
+    public void save(){
+        Intent intent = new Intent(getBaseContext(), LoginActivity.class);
+        startActivity(intent);
+    }
+
 }
+
